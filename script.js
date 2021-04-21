2cc550bcd5e784fb18c7931e0e6845c7b8cead1a
Keano Bessa <keano.rcb@gmail.com>
2021-04-21 23:30:17 +0100

    CSS and HTML refactor

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "strict";
+
+// ///////// ELEMENTS
+// const listPreviewElement = document.querySelector(".list-column");
+// const listItem = document.querySelector(".list-item");
+// const listItemExtraInfo = document.querySelector(".list-additional-info");
+// const newBtn = document.getElementById("btn");
+// const listPreviewElement = document.querySelector(".list-element");
+// const listPreviewElement = document.querySelector(".list-element");
+// const listPreviewElement = document.querySelector(".list-element");
+// const listPreviewElement = document.querySelector(".list-element");
+// const listPreviewElement = document.querySelector(".list-element");
+// const listPreviewElement = document.querySelector(".list-element");
+const newListBtn = document.querySelector(".new-list-btn");
+// const editTitleBtn = document.getElementById(".edit-title-btn");
+// const checkboxBtn = document.querySelector(".check");
+// const expandListItemBtn = document.querySelector(".expand-item-btn");
+// const deleteItemBtn = document.querySelector(".delete-item-btn");
+
+// ///////// EVENTLISTENERS
+newListBtn.addEventListener("click", addNewList);
+// ///////// FUNCTIONS
+function addNewList() {
+  console.log(`hello`);
+}
